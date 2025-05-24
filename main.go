1cfd5df466e8c29c384fa6370a69b4d98503ec7f
santokan <houska.peter@gmail.com>
2025-05-24 21:49:27 +0200

    1-3

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+const inputFile = "messages.txt"
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	f, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer f.Close()
+
+	// read messages 8bits at a time and print them
+	for {
+		b := make([]byte, 8, 8)
+		n, err := f.Read(b)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Printf("error: %s\n", err.Error())
+			break
+		}
+		str := string(b[:n])
+		fmt.Printf("read: %s\n", str)
+	}
 }
