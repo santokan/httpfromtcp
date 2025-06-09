6786f51ec97abe05f973351764e8b75d512dae59
santokan <houska.peter@gmail.com>
2025-06-09 17:25:39 +0200

    1-5

--- main.go
+++ main.go
@@ -16,32 +16,47 @@ func main() {
 		fmt.Println("Error opening file:", err)
 		return
 	}
-	defer f.Close()
 
 	fmt.Println("Reading messages from file:", inputFile)
 
-	var currentLine string
-	for {
-		b := make([]byte, 8, 8)
-		n, err := f.Read(b)
-		if err != nil {
-			if currentLine != "" {
-				fmt.Printf("read: %s\n", currentLine)
-				currentLine = ""
-			}
-			if errors.Is(err, io.EOF) {
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(lines)
+
+		var currentLine string
+		for {
+			b := make([]byte, 8)
+			n, err := f.Read(b)
+			if err != nil {
+				if currentLine != "" {
+					lines <- currentLine
+					currentLine = ""
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
 				break
 			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
+			str := string(b[:n])
+			parts := strings.Split(str, "\n")
+			for i := range len(parts) - 1 {
+				currentLine += parts[i]
+				lines <- currentLine
+				currentLine = ""
+			}
+			currentLine += parts[len(parts)-1]
 		}
-		str := string(b[:n])
-		parts := strings.Split(str, "\n")
-		for i := range len(parts) - 1 {
-			currentLine += parts[i]
-			fmt.Printf("read: %s\n", currentLine)
-			currentLine = ""
-		}
-		currentLine += parts[len(parts)-1]
-	}
+	}()
+
+	return lines
 }
